22384abca61d1bf1afce781d0ff35f304b6a083a
katiijay <katherine.pillsbury@gmail.com>
2025-08-02 13:31:53 -0700

    adding API calls to microservice to return real values

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,13 @@
 from app import app
 from flask import jsonify, request
+from .calls.weathercall import get_weather
 
-test_payload = [
-    {
-  'temperature': {
-    'min': 58,
-    'max': 72,
-    'scale': 'fahrenheit'
-  },
-  'cloud': {
-    'coverage': 15,
-    'scale': 'percentage'
-  },
-  'precipitation': {
-    'type': 'rain',
-    'amount': 5,
-    'scale': 'inches'
-  },
- 'wind': {
-    'speed': 10,
-    'scale': 'mph'
-  }
-}
-]
-
-@app.route('/weather')
-def get_incomes():
-    return jsonify(test_payload)
+@app.route('/forecast', methods=['GET'])
+def get_forecast():
+    latitude = request.args.get('lat')
+    longitude = request.args.get('long')
+    date = request.args.get('date')
+    results = get_weather(latitude, longitude, date)
+    #if date < datetime.today():
+    #    raise ValueError('Cannot retrieve a forecast for a day that has already occurred')
+    return jsonify(results)
